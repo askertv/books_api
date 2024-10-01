9ae5b6ff60e8022a80ed1d0b2a49e784254cafbc
AskerT <47038992+askertv@users.noreply.github.com>
2024-10-01 03:08:03 +0300

    Replace tabs to spaces

--- main.go
+++ main.go
@@ -18,51 +18,51 @@ import (
 const uri = "mongodb://localhost"
 
 type Book struct {
-	Id primitive.ObjectID `bson:"_id"`
-	Book string `bson:"book"`
+    Id primitive.ObjectID `bson:"_id"`
+    Book string `bson:"book"`
     Author_id primitive.ObjectID `bson:"author_id"`
     Published_year int `bson:"published_year"`
-	Published_city string `bson:"published_city"`
+    Published_city string `bson:"published_city"`
 }
 
 func ShowBooks(writer http.ResponseWriter, request *http.Request) {
     serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+    opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
-	client, err := mongo.Connect(context.TODO(), opts)
-	if err != nil {
-		panic(err)
-	}
+    client, err := mongo.Connect(context.TODO(), opts)
+    if err != nil {
+        panic(err)
+    }
 
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+    defer func() {
+        if err = client.Disconnect(context.TODO()); err != nil {
+            panic(err)
+        }
+    }()
 
-	coll := client.Database("maindb").Collection("books")
+    coll := client.Database("maindb").Collection("books")
 
     filter := bson.D{{ "book", bson.D{{ "$exists", true}} }}
 
-	cursor, err := coll.Find(context.TODO(), filter)
-	if err != nil {
-		panic(err)
-	}
+    cursor, err := coll.Find(context.TODO(), filter)
+    if err != nil {
+        panic(err)
+    }
 
     var results []Book
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
+    if err = cursor.All(context.TODO(), &results); err != nil {
+        panic(err)
+    }
 
     for _, result := range results {
-		cursor.Decode(&result)
-		output, err := json.MarshalIndent(result, "", "    ")
-		if err != nil {
-			panic(err)
-		}
+        cursor.Decode(&result)
+        output, err := json.MarshalIndent(result, "", "    ")
+        if err != nil {
+            panic(err)
+        }
 
         io.WriteString(writer, fmt.Sprintf("%s\n", output))
-	}
+    }
 }
 
 func main() {
